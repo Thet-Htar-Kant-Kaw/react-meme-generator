51dbee8fa3495221413a2b6605c33fe85843f856
Tea-Tong <thethtarkantkaw95@gmail.com>
2023-12-28 22:49:13 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header'
+import Meme from './components/Meme'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+export default App
